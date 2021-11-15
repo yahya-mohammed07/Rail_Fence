9b33c1989c861d37b92e499cc1021d38ef5d36a0
yahya-mohammed07 <goldroger300@gmail.com>
2021-11-15 14:08:23 +0300

    updated

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Rail_Fence
@@ -39,7 +33,7 @@ namespace Rail_Fence
       int key = int.Parse(Key.Text);
       string temp = Plain_text.Text;
       //
-      if ( Reverse.Checked )
+      if (Reverse.Checked)
       {
         temp = ReverseXor(temp);
       }
